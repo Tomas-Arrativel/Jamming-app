e415547d7d0ea44ea278fe91645360451df1e10b
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-10 15:16:16 -0300

    Made the basic logic of the search component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import styles from './Searchbar.module.css';
+import SearchBtn from '../SearchBtn/SearchBtn';
 
 const Searchbar = () => {
-  return <div>Searchbar</div>;
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => setValue(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' value={value} onChange={handleChange} />
+      <input type='submit' value='Search' />
+    </form>
+  );
 };
 
 export default Searchbar;
