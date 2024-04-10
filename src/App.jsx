1029c4d1105bb6e4bc2b8730349b00e392a540a9
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-10 15:07:30 -0300

    Making the structure that i think that is ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import style from './App.module.css';
+import Searchbar from './components/Searchbar/Searchbar';
+import SearchResults from './components/SearchResults/SearchResults';
+import Tracklist from './components/Tracklist/Tracklist';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>
+          Ja<span>mmm</span>ing
+        </h1>
+      </header>
+      <main>
+        <Searchbar />
+        <div>
+          <SearchResults />
+          <Tracklist />
+        </div>
+      </main>
+      <footer>
+        <p>Copyright 2024 Tomás Arrativel</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
