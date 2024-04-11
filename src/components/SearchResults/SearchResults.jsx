853b3025d497348a458408aa5b7d975bc49cedf8
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-11 15:33:51 -0300

    Initial structure fo the searchResults component

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,7 +1,21 @@
 import styles from './SearchResults.module.css';
+import Track from '../Track/Track';
 
 const SearchResults = () => {
-  return <div>SearchResults</div>;
+  return (
+    <div className={styles['results']}>
+      <h2>Results</h2>
+      <div className={styles['results__container']}>
+        <Track title='Given up' artist='Linkin Park' album='Nose xd' />
+        <Track
+          title='Nothing Else Matters'
+          artist='Metallica'
+          album='Nose xd'
+        />
+        <Track title='Thunderstruck' artist='AcDc' album='Nose xd' />
+      </div>
+    </div>
+  );
 };
 
 export default SearchResults;
