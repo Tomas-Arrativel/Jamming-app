3ef6c0a4f015550170f07c61904ff636fd4cdd35
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-11 18:24:05 -0300

    Track component structure finished

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,7 +1,16 @@
 import styles from './Track.module.css';
 
-const Track = () => {
-  return <div>Track</div>;
+const Track = ({ title, artist, album }) => {
+  return (
+    <div className={styles['track']}>
+      <h3 className={styles['track__title']}>{title}</h3>
+      <p>
+        {artist}
+        <span>|</span>
+        {album}
+      </p>
+    </div>
+  );
 };
 
 export default Track;
