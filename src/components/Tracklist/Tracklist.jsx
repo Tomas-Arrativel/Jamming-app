f3948d6ea26ec74c68220360846572224152fb79
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-15 16:52:08 -0300

    Tracklist component finished and principal structure of the app finished

--- src/components/Tracklist/Tracklist.jsx
+++ src/components/Tracklist/Tracklist.jsx
@@ -1,7 +1,24 @@
+import Track from '../Track/Track';
 import styles from './Tracklist.module.css';
 
 const Tracklist = () => {
-  return <div>Tracklist</div>;
+  return (
+    <div className={styles['selected']}>
+      <input
+        type='text'
+        placeholder='Playlist name'
+        className={styles['playlist__name']}
+      />
+      <div className={styles['selected__songs']}>
+        <Track
+          album={'nose'}
+          artist={"Guns 'n Roses"}
+          title={"Sweet Child 'o Mine"}
+        />
+      </div>
+      <button className={styles['saveto__btn']}>Save to Spotify</button>
+    </div>
+  );
 };
 
 export default Tracklist;
