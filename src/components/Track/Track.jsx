4a4980ccddd86a6849ead08ae9986f0a7f30f515
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-15 15:33:55 -0300

    Adding some features to the track component

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -3,12 +3,15 @@ import styles from './Track.module.css';
 const Track = ({ title, artist, album }) => {
   return (
     <div className={styles['track']}>
-      <h3 className={styles['track__title']}>{title}</h3>
-      <p>
-        {artist}
-        <span>|</span>
-        {album}
-      </p>
+      <div className={styles['track__info']}>
+        <h3>{title}</h3>
+        <p>
+          {artist}
+          <span>|</span>
+          {album}
+        </p>
+      </div>
+      <button className={styles['track__btn']}>+</button>
     </div>
   );
 };
