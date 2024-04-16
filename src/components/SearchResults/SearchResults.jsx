026abd74c1e8c134957651d78f5f3e05f46f6cac
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-16 18:57:46 -0300

    Creating the functionality of the results getting displayed

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,18 +1,25 @@
 import styles from './SearchResults.module.css';
 import Track from '../Track/Track';
 
-const SearchResults = () => {
+const SearchResults = ({ searchResults }) => {
   return (
     <div className={styles['results']}>
       <h2>Results</h2>
       <div className={styles['results__container']}>
-        <Track title='Given up' artist='Linkin Park' album='Nose xd' />
-        <Track
-          title='Nothing Else Matters'
-          artist='Metallica'
-          album='Nose xd'
-        />
-        <Track title='Thunderstruck' artist='AcDc' album='Nose xd' />
+        {searchResults.length > 0 ? (
+          searchResults.map((result) => (
+            <Track
+              title={result.name}
+              artist={result.artist}
+              album={result.album}
+              key={result.id}
+            />
+          ))
+        ) : (
+          <p className={styles['results__container-noSearch']}>
+            Search something to get some results
+          </p>
+        )}
       </div>
     </div>
   );
