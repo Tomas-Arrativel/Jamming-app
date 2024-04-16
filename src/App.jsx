ade4503dac2d91e20af8259ba1c30c91fa6bdff2
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-16 15:23:34 -0300

    Creating the temporal tracks data array

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,51 @@ import Searchbar from './components/Searchbar/Searchbar';
 import SearchResults from './components/SearchResults/SearchResults';
 import Tracklist from './components/Tracklist/Tracklist';
 
+export const data = [
+  {
+    name: 'Flaca',
+    artist: 'Andres Calamaro',
+    album: 'Alta suciedad',
+    id: 1,
+  },
+  {
+    name: 'Loco',
+    artist: 'Andres Calamaro',
+    album: 'Alta suciedad',
+    id: 2,
+  },
+  {
+    name: 'Tan solo',
+    artist: 'Los Piojos',
+    album: 'Chac tu chac',
+    id: 3,
+  },
+  {
+    name: 'Mirenla',
+    artist: 'Ciro y los persas',
+    album: '27',
+    id: 4,
+  },
+  {
+    name: 'Bicho de ciudad',
+    artist: 'Los Piojos',
+    album: 'Civilización',
+    id: 5,
+  },
+  {
+    name: 'ASTROS',
+    artist: 'Ciro y Los Persas',
+    album: '27',
+    id: 6,
+  },
+  {
+    name: 'A Las Nueve',
+    artist: 'No Te Va Gustar',
+    album: 'El Calor Del Pleno Invierno',
+    id: 7,
+  },
+];
+
 function App() {
   return (
     <>
