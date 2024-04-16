b2b1b10e9310a94c72849760b3fdae7b8fe345ec
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-16 18:47:56 -0300

    Creating the functionality of the search component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react';
 import style from './Searchbar.module.css';
 
-const Searchbar = () => {
+//get the tracks from the app component
+import { data } from '../../App';
+
+const Searchbar = ({ setSearchResults }) => {
   const [value, setValue] = useState('');
+
+  const getResults = (search) => {
+    setSearchResults([]);
+    data.map((track) =>
+      track.name.toLowerCase().includes(search.toLowerCase())
+        ? setSearchResults((prevResults) => [...prevResults, track])
+        : '',
+    );
+  };
 
   const handleChange = (e) => setValue(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    getResults(value);
     setValue('');
   };
 
