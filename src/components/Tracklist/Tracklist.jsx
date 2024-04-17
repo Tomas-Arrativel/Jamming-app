b959007c3b81c3e18d82ee5e3bbd4941f9bbfc95
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-17 18:31:14 -0300

    Using and creating the functions to create the playlist

--- src/components/Tracklist/Tracklist.jsx
+++ src/components/Tracklist/Tracklist.jsx
@@ -1,11 +1,37 @@
+import { useState } from 'react';
 import Track from '../Track/Track';
 import styles from './Tracklist.module.css';
 
-const Tracklist = ({ addedSongs, setAddedSongs }) => {
+const Tracklist = ({ addedSongs, setAddedSongs, setPlaylists }) => {
+  const [playlistName, setPlaylistName] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleOnChange = (e) => {
+    setPlaylistName(e.target.value);
+  };
+
+  const handleCreatePlaylist = () => {
+    if (playlistName.trim() === '' || addedSongs.length < 1) {
+      setError(true);
+      return;
+    }
+
+    setPlaylists((prevPlaylists) => [
+      ...prevPlaylists,
+      { name: playlistName, songs: addedSongs },
+    ]);
+
+    setAddedSongs([]);
+    setPlaylistName('');
+    setError(false);
+  };
+
   return (
     <div className={styles['selected']}>
       <input
         type='text'
+        value={playlistName}
+        onChange={handleOnChange}
         placeholder='Playlist name'
         className={styles['playlist__name']}
       />
@@ -25,8 +51,17 @@ const Tracklist = ({ addedSongs, setAddedSongs }) => {
         ) : (
           <p>Add some songs by clicking the + button</p>
         )}
+        {error ? (
+          <p className={styles['error']}>
+            Pass at least 1 song and a name to your playlist
+          </p>
+        ) : (
+          ''
+        )}
       </div>
-      <button className={styles['saveto__btn']}>Save to Spotify</button>
+      <button className={styles['saveto__btn']} onClick={handleCreatePlaylist}>
+        Save to Spotify
+      </button>
     </div>
   );
 };
