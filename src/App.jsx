824855230cfb08a88517c11772aa2d6c17bba0aa
Tomás Arrativel <arrativeltomas@gmail.com>
2024-04-18 17:10:00 -0300

    transforming the playlists state to a single playlist state

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import style from './App.module.css';
 import Searchbar from './components/Searchbar/Searchbar';
 import SearchResults from './components/SearchResults/SearchResults';
 import Tracklist from './components/Tracklist/Tracklist';
-import Playlist from './components/Playlist/Playlist';
 
 export const data = [
   {
@@ -53,7 +52,7 @@ export const data = [
 function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [addedSongs, setAddedSongs] = useState([]);
-  const [playlists, setPlaylists] = useState([]);
+  const [playlist, setPlaylist] = useState({});
 
   return (
     <>
@@ -72,10 +71,9 @@ function App() {
           <Tracklist
             addedSongs={addedSongs}
             setAddedSongs={setAddedSongs}
-            setPlaylists={setPlaylists}
+            setPlaylist={setPlaylist}
           />
         </div>
-        <Playlist playlists={playlists} />
       </main>
       <footer className={style['footer']}>
         <p>Copyright 2024 Tomás Arrativel</p>
